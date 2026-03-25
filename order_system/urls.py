208f0347b86026b7566f7b63b5d30b3f991084f9
11346063 <11346063@ntub.edu.tw>
2026-03-25 11:30:52 +0800

    0325-3

--- order_system/urls.py
+++ order_system/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from web_app.views.type import type_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+]
+
+urlpatterns += [
+    path("type/create/", type_views.typeCreate),
 ]
---
+++ web_app/views/type/create.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from ...forms.type_form import TypeForm
+from ...models.type import Type
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
+
+
+def announcementCreate(request):
+    form = TypeForm()
+
+    if request.method == "POST":
+        form = TypeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        
+    return render(request, "type/create.html", {"form": form})
