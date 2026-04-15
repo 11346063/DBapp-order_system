0abb7adde85746294c5d776f04d6e60ef25b0d78
11346063 <11346063@ntub.edu.tw>
2026-04-15 10:17:15 +0800

    修改: 修正遺漏的權限bug

--- web_app/views/auth_views.py
+++ web_app/views/auth_views.py
@@ -19,7 +19,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, f'歡迎回來，{user.name}！')
-                if user.is_staff:
+                if user.identity == "A" or user.identity == "E":
                     return redirect('web_app:staff_orders')
                 next_url = request.GET.get('next', 'web_app:home')
                 return redirect(next_url)
