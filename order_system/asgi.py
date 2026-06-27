29d54d526597e197154f5d86e3b76678d925b1c8
LinDragon1002 <lindragon1002@gmail.com>
2026-06-27 23:27:42 +0800

    fix: 修正 asgi.py import 順序，確保 DJANGO_SETTINGS_MODULE 在 Django import 前設定

--- order_system/asgi.py
+++ order_system/asgi.py
@@ -1,12 +1,14 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from order_system import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "order_system.settings")
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack  # noqa: E402
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from order_system import routing  # noqa: E402
 
 
 application = ProtocolTypeRouter(
